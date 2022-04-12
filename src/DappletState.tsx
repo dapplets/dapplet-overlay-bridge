d26c7679a314244a41abcb997b279fc0d9875a38
Nikita Teremovskiy <ni-2@mail.ru>
2022-04-12 15:30:27 +0300

    feat: state.all -> state.global, commonState -> sharedState, changeState -> changeSharedState.

--- src/DappletState.tsx
+++ src/DappletState.tsx
@@ -3,20 +3,20 @@ import { Bridge } from './Bridge';
 import { BehaviorSubjectProxy } from 'rxjs-proxify';
 
 export interface IDappStateProps <T> {
-    commonState: { [name: string]: T }
+    sharedState: { [name: string]: T }
     id?: string
     defaultState?: T
-    changeState?: (newStateData:  Partial<T>, id?: string) => Promise<void>
+    changeSharedState?: (newStateData:  Partial<T>, id?: string) => Promise<void>
 }
 
 function dappletState<T>(WrappedComponent: any) {
-    return class extends React.Component<any, { loading: boolean, commonState: { [contextId: string]: BehaviorSubjectProxy<T> }, id?: string, defaultState?: T }> {
-      bridge = new Bridge<{ changeState: (newStateData: T, id?: string) => Promise<void> }>();
+    return class extends React.Component<any, { loading: boolean, sharedState: { [contextId: string]: BehaviorSubjectProxy<T> }, id?: string, defaultState?: T }> {
+      bridge = new Bridge<{ changeSharedState: (newStateData: T, id?: string) => Promise<void> }>();
   
       constructor(props: any) {
         super(props);
         this.state = {
-          commonState: {},
+          sharedState: {},
           loading: true,
         }
       }
@@ -24,22 +24,22 @@ function dappletState<T>(WrappedComponent: any) {
       componentDidMount() {
         this.bridge.on('onOpen', (id: string) => this.setState({ id, loading: false }));
         this.bridge.on('getDefaultState', (defaultState: T ) => this.setState({ defaultState, loading: false }));
-        this.bridge.on('changeState', (newState: { [contextId: string]: BehaviorSubjectProxy<T> }) => {
-          this.setState({ commonState: { ...this.state.commonState, ...newState  }, loading: false });
+        this.bridge.on('changeSharedState', (newState: { [contextId: string]: BehaviorSubjectProxy<T> }) => {
+          this.setState({ sharedState: { ...this.state.sharedState, ...newState  }, loading: false });
         });
       }
   
       componentWillUnmount() {
           this.bridge.off('onOpen');
           this.bridge.off('getDefaultState');
-          this.bridge.off('changeState');
+          this.bridge.off('changeSharedState');
       }
   
-      changeState = (newStateData: T, id?: string ) => this.bridge.changeState(newStateData, id);
+      changeSharedState = (newStateData: T, id?: string ) => this.bridge.changeSharedState(newStateData, id);
   
       render() {
         return this.state.loading ? <></>
-          :<WrappedComponent commonState={this.state.commonState} id={this.state.id} defaultState={this.state.defaultState} changeState={this.changeState} {...this.props} />;
+          :<WrappedComponent sharedState={this.state.sharedState} id={this.state.id} defaultState={this.state.defaultState} changeSharedState={this.changeSharedState} {...this.props} />;
       }
     }
   }
